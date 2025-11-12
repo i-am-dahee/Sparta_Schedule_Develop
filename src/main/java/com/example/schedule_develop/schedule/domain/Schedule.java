1e3be43bf1972a253b0c6816e75335af92b849a5
i-am-dahee <hxxly02@gmail.com>
2025-11-12 20:35:43 +0900

    [feat] Schedule 엔티티 생성 #1

--- src/main/java/com/example/schedule_develop/schedule/domain/Schedule.java
+++ src/main/java/com/example/schedule_develop/schedule/domain/Schedule.java
@@ -1,4 +1,26 @@
 package com.example.schedule_develop.schedule.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "schedules")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String title;
+    private String content;
+
+    public Schedule(String name, String title, String content) {
+        this.name = name;
+        this.title = title;
+        this.content = content;
+    }
 }
