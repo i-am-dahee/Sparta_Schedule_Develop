9a986442915df3eeca7529dcdd0e3aa946232a2e
i-am-dahee <hxxly02@gmail.com>
2025-11-14 15:54:31 +0900

    [feat] Schedule service 생성 #1

--- src/main/java/com/example/schedule_develop/schedule/domain/Schedule.java
+++ src/main/java/com/example/schedule_develop/schedule/domain/Schedule.java
@@ -24,4 +24,9 @@ public class Schedule extends BaseEntity {
         this.title = title;
         this.content = content;
     }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
