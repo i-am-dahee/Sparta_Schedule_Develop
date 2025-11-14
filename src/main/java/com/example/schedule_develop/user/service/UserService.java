06f85d65e1c847e8bc567a8cdf84e3c17e584f8e
i-am-dahee <hxxly02@gmail.com>
2025-11-14 19:24:47 +0900

    [feat] User service 생성 #3

--- src/main/java/com/example/schedule_develop/user/service/UserService.java
+++ src/main/java/com/example/schedule_develop/user/service/UserService.java
@@ -1,12 +1,97 @@
 package com.example.schedule_develop.user.service;
 
+import com.example.schedule_develop.user.domain.User;
 import com.example.schedule_develop.user.domain.UserRepository;
+import com.example.schedule_develop.user.dto.UserRequest;
+import com.example.schedule_develop.user.dto.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+
+    // 유저 생성
+    @Transactional
+    public UserResponse save(UserRequest request) {
+        User user = new User(
+                request.getName(),
+                request.getEmail()
+        );
+        User savedUser = userRepository.save(user);
+        return new UserResponse(
+                savedUser.getId(),
+                savedUser.getName(),
+                savedUser.getEmail(),
+                savedUser.getCreatedAt(),
+                savedUser.getModifiedAt()
+        );
+    }
+
+    // 유저 전체 조회
+    @Transactional(readOnly = true)
+    public List<UserResponse> getAll() {
+        List<User> users = userRepository.findAll();
+        List<UserResponse> dtos = new ArrayList<>();
+        for (User user : users) {
+            UserResponse dto = new UserResponse(
+                    user.getId(),
+                    user.getName(),
+                    user.getEmail(),
+                    user.getCreatedAt(),
+                    user.getModifiedAt()
+            );
+            dtos.add(dto);
+        }
+        return dtos;
+    }
+
+    // 유저 개별 조회
+    @Transactional(readOnly = true)
+    public UserResponse getOne(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new IllegalStateException("존재하지 않는 유저입니다.")
+        );
+        return new UserResponse(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                user.getCreatedAt(),
+                user.getModifiedAt()
+        );
+    }
+
+    // 유저 수정
+    @Transactional
+    public UserResponse update(Long userId, UserRequest request) {
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new IllegalStateException("존재하지 않는 유저입니다.")
+        );
+        user.update(
+                request.getName(),
+                request.getEmail()
+        );
+        return new UserResponse(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                user.getCreatedAt(),
+                user.getModifiedAt()
+        );
+    }
+
+    // 유저 삭제
+    @Transactional
+    public void delete(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new IllegalStateException("존재하지 않는 유저입니다.")
+        );
+        userRepository.deleteById(userId);
+    }
 }
