590005744ab90ca417a58a8d680d866aac7210c8
i-am-dahee <hxxly02@gmail.com>
2025-11-14 15:53:40 +0900

    [fix] JpaAuditing 추가 #1

--- src/main/java/com/example/schedule_develop/ScheduleDevelopApplication.java
+++ src/main/java/com/example/schedule_develop/ScheduleDevelopApplication.java
@@ -2,8 +2,11 @@ package com.example.schedule_develop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ScheduleDevelopApplication {
 
     public static void main(String[] args) {
