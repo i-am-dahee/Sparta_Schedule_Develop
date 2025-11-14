af093629739c2a95f36e50455648264bb6ead6ec
i-am-dahee <hxxly02@gmail.com>
2025-11-14 17:54:15 +0900

    [feat] User DTO 생성 #3

---
+++ src/main/java/com/example/schedule_develop/user/dto/UserResponse.java
@@ -0,0 +1,23 @@
+package com.example.schedule_develop.user.dto;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class UserResponse {
+
+    private final Long id;
+    private final String name;
+    private final String email;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime updatedAt;
+
+    public UserResponse(Long id, String name, String email, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+}
