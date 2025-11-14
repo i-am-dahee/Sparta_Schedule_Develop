7fbf2989bbda3e65bea265b5d195d7bbef334af3
i-am-dahee <hxxly02@gmail.com>
2025-11-14 17:55:07 +0900

    [feat] User controller 생성 #3

--- src/main/java/com/example/schedule_develop/user/controller/UserController.java
+++ src/main/java/com/example/schedule_develop/user/controller/UserController.java
@@ -1,12 +1,62 @@
 package com.example.schedule_develop.user.controller;
 
+import com.example.schedule_develop.user.dto.UserRequest;
+import com.example.schedule_develop.user.dto.UserResponse;
 import com.example.schedule_develop.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
+
+    // 유저 생성
+    @PostMapping("/users")
+    public ResponseEntity<UserResponse> createUser(
+            @RequestBody UserRequest request
+    ) {
+        UserResponse result = userService.save(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
+
+    // 유저 전체 조회
+    @GetMapping("/users")
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        List<UserResponse> result = userService.getAll();
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    // 유저 개별 조회
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<UserResponse> getUser(
+            @PathVariable Long userId
+    ) {
+        UserResponse result = userService.getOne(userId);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    // 유저 수정
+    @PatchMapping("/users/{userId}")
+    public ResponseEntity<UserResponse> updateUser(
+            @PathVariable Long userId,
+            @RequestBody UserRequest request
+    ) {
+        UserResponse result = userService.update(userId, request);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    // 유저 삭제
+    @DeleteMapping("/users/{userId}")
+    public ResponseEntity<UserResponse> deleteUser(
+            @PathVariable Long userId
+    ) {
+        userService.delete(userId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
