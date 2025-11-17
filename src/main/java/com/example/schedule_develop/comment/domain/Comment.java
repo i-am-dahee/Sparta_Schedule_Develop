fcf5f7f1657365c4e4850c9dacceede4cda9c9a8
i-am-dahee <hxxly02@gmail.com>
2025-11-17 16:10:52 +0900

    [feat] Comment 엔티티 생성 #11

--- src/main/java/com/example/schedule_develop/comment/domain/Comment.java
+++ src/main/java/com/example/schedule_develop/comment/domain/Comment.java
@@ -1,5 +1,7 @@
 package com.example.schedule_develop.comment.domain;
 
+import com.example.schedule_develop.schedule.domain.Schedule;
+import com.example.schedule_develop.user.domain.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -14,4 +16,21 @@ public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule;
+
+    public Comment(String content, User user, Schedule schedule) {
+        this.content = content;
+        this.user = user;
+        this.schedule = schedule;
+    }
 }
