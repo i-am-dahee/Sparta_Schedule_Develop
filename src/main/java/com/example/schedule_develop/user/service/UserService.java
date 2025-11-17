4dee5253f4014532358451270d46d58e4881250b
i-am-dahee <hxxly02@gmail.com>
2025-11-17 12:30:34 +0900

    [refactor] 필요 없는 코드 제거 #5

--- src/main/java/com/example/schedule_develop/user/service/UserService.java
+++ src/main/java/com/example/schedule_develop/user/service/UserService.java
@@ -8,7 +8,6 @@ import com.example.schedule_develop.user.dto.UserRequest;
 import com.example.schedule_develop.user.dto.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
