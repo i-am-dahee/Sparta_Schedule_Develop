326c807cb94ae33071d0329fb0fe8c31ec16130a
i-am-dahee <hxxly02@gmail.com>
2025-11-17 12:26:44 +0900

    [refactor] DB 제약 조건 설정 #5

--- src/main/java/com/example/schedule_develop/schedule/domain/Schedule.java
+++ src/main/java/com/example/schedule_develop/schedule/domain/Schedule.java
@@ -16,7 +16,11 @@ public class Schedule extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false)
     private String content;
 
     @ManyToOne(fetch = FetchType.LAZY)
--- src/main/java/com/example/schedule_develop/user/domain/User.java
+++ src/main/java/com/example/schedule_develop/user/domain/User.java
@@ -15,8 +15,14 @@ public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String password;
 
     public User(String name, String email, String password) {
