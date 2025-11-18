4174050f22882408dd3b2181e611bd47f87bacf6
i-am-dahee <hxxly02@gmail.com>
2025-11-18 11:16:36 +0900

    [feat] 일정 페이징 조회 기능 추가 #13

--- src/main/java/com/example/schedule_develop/comment/domain/CommentRepository.java
+++ src/main/java/com/example/schedule_develop/comment/domain/CommentRepository.java
@@ -1,6 +1,9 @@
 package com.example.schedule_develop.comment.domain;
 
+import com.example.schedule_develop.schedule.domain.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    Long countByScheduleId(Long scheduleId);
 }
--- src/main/java/com/example/schedule_develop/schedule/service/ScheduleService.java
+++ src/main/java/com/example/schedule_develop/schedule/service/ScheduleService.java
@@ -1,13 +1,18 @@
 package com.example.schedule_develop.schedule.service;
 
+import com.example.schedule_develop.comment.domain.CommentRepository;
 import com.example.schedule_develop.schedule.domain.Schedule;
 import com.example.schedule_develop.schedule.domain.ScheduleRepository;
 import com.example.schedule_develop.schedule.dto.CreateScheduleRequest;
+import com.example.schedule_develop.schedule.dto.PageScheduleResponse;
 import com.example.schedule_develop.schedule.dto.ScheduleResponse;
 import com.example.schedule_develop.schedule.dto.UpdateScheduleRequest;
 import com.example.schedule_develop.user.domain.User;
 import com.example.schedule_develop.user.domain.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +25,7 @@ public class ScheduleService {
 
     private final ScheduleRepository scheduleRepository;
     private final UserRepository userRepository;
+    private final CommentRepository commentRepository;
 
     // 일정 생성
     @Transactional
@@ -62,6 +68,25 @@ public class ScheduleService {
         return dtos;
     }
 
+    // 일정 페이징 조회
+    @Transactional(readOnly = true)
+    public Page<PageScheduleResponse> getPages(Pageable pageable) {
+        Page<Schedule> schedules = scheduleRepository.findAll(pageable);
+        List<PageScheduleResponse> dtos = schedules.getContent().stream()
+                .map(schedule -> new PageScheduleResponse(
+                        schedule.getId(),
+                        schedule.getTitle(),
+                        schedule.getContent(),
+                        commentRepository.countByScheduleId(schedule.getId()),
+                        schedule.getCreatedAt(),
+                        schedule.getModifiedAt(),
+                        schedule.getUser().getName()
+                ))
+                .toList();
+        return new PageImpl<>(dtos, pageable, schedules.getTotalElements());
+    }
+
+
     // 일정 개별 조회
     @Transactional(readOnly = true)
     public ScheduleResponse getOne(Long scheduleId) {
@@ -79,7 +104,6 @@ public class ScheduleService {
     }
 
     // 일정 수정
-    // TODO : 수정 시간 바로 반영되는지 확인
     @Transactional
     public ScheduleResponse update(UpdateScheduleRequest request, Long scheduleId) {
         Schedule schedule = scheduleRepository.findById(scheduleId).orElseThrow(
